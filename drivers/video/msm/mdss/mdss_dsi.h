16388b5031912f90694ea751ac0cf88e19647a5b
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-02-21 11:59:51 +0530

    msm: mdss: add a separate register region for DSI PHY
    The DSI PHY register offset from the controller base is
    different for different targets. So, add a separate register
    region for DSI PHY in the device tree. This register base and
    offset will be used to access the DSI PHY registers. Also
    change the register offsets which are dependent on the
    controller base address.
    
    Change-Id: Ie1d08950f3c8c8801908a1b3cf7db46a44b4e8c3
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -329,7 +329,9 @@ struct mdss_dsi_ctrl_pdata {
 	int (*off) (struct mdss_panel_data *pdata);
 	struct mdss_panel_data panel_data;
 	unsigned char *ctrl_base;
+	struct dss_io_data ctrl_io;
 	struct dss_io_data mmss_misc_io;
+	struct dss_io_data phy_io;
 	int reg_size;
 	u32 clk_cnt;
 	int clk_cnt_sub;
