b598808e592d092db0995cacd09ceaae6c98e42e
Jayant Shekhar <jshekhar@codeaurora.org>
2014-05-12 11:42:20 +0530

    msm: mdss: Calculate plane size correctly for H2V1/H1V2 rotation
    Currently, chroma sample is calculated after calculation of plane
    size and stride in case of rotation in H2V1/H1V2 formats. Fix
    this to correct chroma sample before plane sizes and stride are
    calculated.
    
    Change-Id: I851d61034b465d48fd21bb4a015c103457dc7936
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -265,7 +265,7 @@ int mdss_mdp_smp_reserve(struct mdss_mdp_pipe *pipe)
 			}
 		}
 		rc = mdss_mdp_get_plane_sizes(format, width, pipe->src.h,
-			&ps, 0);
+			&ps, 0, 0);
 		if (rc)
 			return rc;
 
@@ -872,6 +872,7 @@ static int mdss_mdp_image_setup(struct mdss_mdp_pipe *pipe)
 	u32 img_size, src_size, src_xy, dst_size, dst_xy, ystride0, ystride1;
 	u32 width, height;
 	u32 decimation;
+	bool rotation = false;
 
 	pr_debug("pnum=%d wh=%dx%d src={%d,%d,%d,%d} dst={%d,%d,%d,%d}\n",
 		   pipe->num, pipe->img_width, pipe->img_height,
@@ -880,8 +881,12 @@ static int mdss_mdp_image_setup(struct mdss_mdp_pipe *pipe)
 
 	width = pipe->img_width;
 	height = pipe->img_height;
+
+	if (pipe->flags & MDP_SOURCE_ROTATED_90)
+		rotation = true;
+
 	mdss_mdp_get_plane_sizes(pipe->src_fmt->format, width, height,
-			&pipe->src_planes, pipe->bwc_mode);
+			&pipe->src_planes, pipe->bwc_mode, rotation);
 
 	if ((pipe->flags & MDP_DEINTERLACE) &&
 			!(pipe->flags & MDP_SOURCE_ROTATED_90)) {
