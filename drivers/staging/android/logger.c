02dfc5d6bad1ca1b472c36a8fcf9250c370c8056
Dan Trifan <jimsth@gmail.com>
2014-05-28 17:41:34 +0300

    logger: modify the sysfs for the enable toggle
     * You can disable logging by echo-ing 0 in /sys/module/logger/parameters/enabled
     * By default the logging is enabled.
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -33,8 +33,8 @@
 #define CONFIG_LOGCAT_SIZE 256
 #endif
 
-static unsigned int log_enabled = 1;
-module_param(log_enabled, uint, S_IWUSR | S_IRUGO);
+static unsigned int enabled = 1;
+module_param(enabled, uint, S_IWUSR | S_IRUGO);
 
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
@@ -473,7 +473,7 @@ ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct timespec now;
 	ssize_t ret = 0;
 
-    if (!log_enabled)
+    if (!enabled)
         return 0;
 
 	now = current_kernel_time();
